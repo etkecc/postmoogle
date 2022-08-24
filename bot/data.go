etkecc/postmoogle

commit 726bc95c26d907151e44e9760dcd3eb4e2699272

Suppress M_NOT_FOUND error when fetching settings
.. and ensure that getSettings() always returns either a default empty
settings object or an error.

bot/data.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"context"
+	"strings"
 
 	"github.com/getsentry/sentry-go"
 	"maunium.net/go/mautrix/id"
@@ -65,6 +66,15 @@ func (b *Bot) getSettings(ctx context.Context, roomID id.RoomID) (settings, erro
 
 	var config settings
 	err := b.lp.GetClient().GetRoomAccountData(roomID, settingskey, &config)
+
+	if err != nil {
+		if strings.Contains(err.Error(), "M_NOT_FOUND") {
+			// Suppress `M_NOT_FOUND (HTTP 404): Room account data not found` errors.
+			// Until some settings are explicitly set, we don't store any.
+			// In such cases, just return a default (empty) settings object.
+			err = nil
+		}
+	}
 
 	return config, err
 }

bot/mailbox.go
@@ -50,6 +50,8 @@ func (b *Bot) getMailbox(ctx context.Context, evt *event.Event) {
 	cfg, err := b.getSettings(span.Context(), evt.RoomID)
 	if err != nil {
 		b.log.Warn("cannot get %s settings: %v", evt.RoomID, err)
+		b.Error(span.Context(), evt.RoomID, "failed to retrieve settings")
+		return
 	}
 
 	if cfg.Mailbox == "" {
@@ -82,6 +84,8 @@ func (b *Bot) setMailbox(ctx context.Context, evt *event.Event, mailbox string) 
 	cfg, err := b.getSettings(span.Context(), evt.RoomID)
 	if err != nil {
 		b.log.Warn("cannot get settings: %v", err)
+		b.Error(span.Context(), evt.RoomID, "failed to retrieve settings")
+		return
 	}
 
 	if !cfg.Allowed(b.noowner, evt.Sender) {

bot/owner.go
@@ -23,7 +23,8 @@ func (b *Bot) getOwner(ctx context.Context, evt *event.Event) {
 
 	cfg, err := b.getSettings(span.Context(), evt.RoomID)
 	if err != nil {
-		b.Error(span.Context(), evt.RoomID, "owner is not set yet")
+		b.log.Warn("cannot get settings: %v", err)
+		b.Error(span.Context(), evt.RoomID, "failed to retrieve settings")
 		return
 	}
 
@@ -48,6 +49,8 @@ func (b *Bot) setOwner(ctx context.Context, evt *event.Event, owner string) {
 	cfg, err := b.getSettings(span.Context(), evt.RoomID)
 	if err != nil {
 		b.log.Warn("cannot get settings: %v", err)
+		b.Error(span.Context(), evt.RoomID, "failed to retrieve settings")
+		return
 	}
 
 	if !cfg.Allowed(b.noowner, evt.Sender) {

commit ed5ff6456a5385fc238c3282d757017571ce9802

Ensure settings map is always initialized
Without this, if settings are not found in account data (`M_NOT_FOUND`),
we won't initialize the map and we'll panic later:

> assignment to entry in nil map

Breaking settings for all new rooms is definitely not what we inteded.

Likely a regression since 726bc95c26d90, but related to fcac0a202d84 as
well.

bot/data.go
@@ -161,7 +161,7 @@ func (b *Bot) getSettings(ctx context.Context, roomID id.RoomID) (settings, erro
 	span := sentry.StartSpan(ctx, "http.server", sentry.TransactionName("getSettings"))
 	defer span.Finish()
 
-	var config settings
+	config := settings{}
 	err := b.lp.GetClient().GetRoomAccountData(roomID, settingskey, &config)
 	if err != nil {
 		if strings.Contains(err.Error(), "M_NOT_FOUND") {